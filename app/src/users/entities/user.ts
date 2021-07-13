f27d6aa11ebbfc22de837ad0c33faa7c11ba2ea3
Rodolfo <rodolfo.coding@gmail.com>
2021-07-13 17:52:18 -0300

    Iniciando serviço de usuário

---
+++ app/src/users/entities/user.ts
@@ -0,0 +1,20 @@
+import { randomUUID } from 'crypto';
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+
+@Entity()
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  password: string;
+}
---
+++ app/src/users/users.controller.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, Get, OnModuleInit, Post } from '@nestjs/common';
+import { ApiBody } from '@nestjs/swagger';
+import { UserDto } from "./DTO's/user.dto";
+import { UserEntity } from './entities/user';
+import { IUser } from './interfaces/user.interface';
+import { Client, ClientKafka, Transport } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+
+@Controller('users')
+export class UsersController implements OnModuleInit {
+  @Client({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'user',
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'user-consumer',
+        allowAutoTopicCreation: true,
+      },
+    },
+  })
+  private client: ClientKafka;
+
+  async onModuleInit() {
+    const requestPatters = ['find-all-user', 'create-user'];
+
+    requestPatters.map(async (pattern) => {
+      this.client.subscribeToResponseOf(pattern);
+      await this.client.connect();
+    });
+  }
+
+  @Get()
+  index(): Observable<UserEntity[]> {
+    return this.client.send('find-all-user', {});
+  }
+
+  @Post()
+  @ApiBody({ type: UserDto })
+  create(@Body() user: IUser): Observable<UserEntity> {
+    return this.client.send('create-user', user);
+  }
+}
